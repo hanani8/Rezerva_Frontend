e789cb4bc8dfbe829ee9d6d078f80797bc8663f5
hanani8 <hananidasilva@gmail.com>
2022-11-15 11:53:23 +0530

    Added "Confirm" button to card. Added "W" identifier to waitlisted reservations

--- src/lib/card.svelte
+++ src/lib/card.svelte
@@ -3,11 +3,11 @@
 
     import { patchFetch } from "$lib/fetch";
 
+    import expand from "../assets/expand.svg";
+
     import { goto } from "$app/navigation";
 
     export let reservation;
-
-    let status = reservation["status"];
 
     async function cancel(id) {
         const URL = `${url}/api/reservation/${id}`;
@@ -17,7 +17,21 @@
         const data = await patchFetch(URL, JSON.stringify(formData));
 
         if (data) {
-            status = 2;
+            reservation["status"] = 2;
+        }
+    }
+
+    async function confirm(id) {
+        const URL = `${url}/api/reservation/${id}`;
+
+        const formData = {
+            status: 0,
+        };
+
+        const data = await patchFetch(URL, JSON.stringify(formData));
+
+        if (data) {
+            reservation["status"] = 0;
         }
     }
 
@@ -61,57 +75,131 @@
     }
 </script>
 
-<div class="relative w-full overflow-auto rounded-lg bg-gray-200 px-2 py-2">
+<div class="relative h-full w-full rounded-lg">
     <!-- reservation or walk-in -->
 
     <div class="float-right flex flex-col gap-2">
-        <div class="mt-2 mr-2 bg-black px-2 text-center text-white">
-            {#if reservation["type"] == "1"}
-                reservation
-            {:else if reservation["type"] == "2"}
-                walk-in
+        <div class="absolute right-0 bg-black px-1 text-center text-white">
+            {#if reservation["status"] == "1"}
+                W
+            {/if}
+        </div>
+    </div>
+
+    <div class="flex h-full flex-col gap-3 overflow-auto border-2 border-black">
+        <div class="flex h-full flex-row items-center  justify-evenly">
+            <div class="flex flex-col">
+                <span class="text-sm font-medium"
+                    >{reservation["guest_name"]}</span
+                >
+                <span class="font-mono text-sm">
+                    {reservation["phone"]}
+                </span>
+            </div>
+
+            <div class="flex flex-col">
+                <span class="text-sm"> People </span>
+                <span class="text-center text-sm"
+                    >{reservation["no_of_guests"]}</span
+                >
+            </div>
+
+            <div class="flex flex-col">
+                <span class="text-sm"> Time </span>
+                <span class="text-sm">
+                    {humanizeDate(reservation["reservation_time"])[
+                        "reservation_time"
+                    ]}
+                </span>
+            </div>
+
+            <div class="flex flex-col">
+                <span class="text-sm"> Table </span>
+                <span class="text-sm">
+                    {#if reservation["table"] != ""}
+                        {reservation["table"]}
+                    {:else}
+                        ~
+                    {/if}
+                </span>
+            </div>
+
+            {#if reservation["status"] != 2}
+                <div class="">
+                    <a href="/edit/{reservation['reservation_id']}">
+                        <!-- The reason for button is to disable the redirection to edit page if the reservation is already cancelled -->
+                        <!-- https://stackoverflow.com/questions/34684467/dont-redirect-on-button-click-with-css -->
+                        <button type="button">
+                            <span class="text-sm text-green-800"> Edit </span>
+                        </button>
+                    </a>
+                </div>
+
+                <div class="">
+                    <button
+                        on:click={() => {
+                            if (reservation["status"] != 2) {
+                                cancel(reservation["reservation_id"]);
+                            }
+                        }}
+                    >
+                        <span class="text-sm text-red-800"> Cancel </span>
+                    </button>
+                </div>
+                {#if reservation["status"] == 1}
+                    <div class="">
+                        <button
+                            on:click={() => {
+                                if (
+                                    reservation["status"] != 2 &&
+                                    reservation["status"] == 1
+                                ) {
+                                    confirm(reservation["reservation_id"]);
+                                }
+                            }}
+                        >
+                            <span class="text-fuchisia-800 text-sm">
+                                Confirm
+                            </span>
+                        </button>
+                    </div>
+                {/if}
+            {:else}
+                <div class="text-md tracking-wide">CANCELLED</div>
             {/if}
         </div>
 
-        {#if status !== 2}
-            <div class="mr-2 bg-amber-200 text-center ">
-                <a href="/edit/{reservation['reservation_id']}">edit</a>
-            </div>
+        <div class="mb-3 w-4/5 self-center border border-black ">
+            <span class="text-center text-sm">
+                {reservation["instructions"]}
+            </span>
+        </div>
 
-            <div class="mr-2 bg-red-500 text-center text-white">
+        <!-- <div class="relative">
                 <button
                     on:click={() => {
-                        cancel(reservation["reservation_id"]);
-                    }}>cancel</button
+                        document
+                            .getElementById("dropdown")
+                            .classList.remove("hidden");
+                        document
+                            .getElementById("dropdown")
+                            .classList.add("flex flex-col");
+                    }}
                 >
+                    <img class="h-8" src={expand} alt="Expand More Icon" />
+                </button>
+                <div id="dropdown" class="w-24 absolute hidden z-50">
+                    <a class="block" href="/edit">Link 1</a>
+                    <a class="block" href="/cancel">Link 2</a>
+                </div>
             </div>
-        {:else}
-            <div class="mr-2 bg-red-200 text-center ">
-                <span>cancelled</span>
-            </div>
-        {/if}
-    </div>
+        </div> -->
 
-    <p class="font-sans text-lg font-medium">{reservation["guest_name"]}</p>
-
-    <p class="font-mono text-lg font-bold">
-        <span class="">Guests: {reservation["no_of_guests"]}</span>
-    </p>
-
-    <p class="font-mono text-xl font-semibold">
-        {humanizeDate(reservation["reservation_time"])["reservation_date"]}
-    </p>
-
-    <p class="font-mono text-xl font-semibold">
-        {humanizeDate(reservation["reservation_time"])["reservation_time"]}
-    </p>
-
-    <p class="inline-block px-2 font-mono text-lg font-light outline outline-2">
-        {reservation["phone"]}
-    </p>
-
-    <div class="mt-3 font-sans text-base">
-        <span class="">Notes: </span>
-        {reservation["instructions"]}
+        <!-- <div id="instructions" class="flex-row">
+            <span class="text-sm"> Instructions: </span>
+            <span class="text-sm">
+                {reservation["instructions"]}
+            </span>
+        </div> -->
     </div>
 </div>
