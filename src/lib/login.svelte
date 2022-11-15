d6d9aea268df650e677df8be806ea5ddbe6524c8
hanani8 <hananidasilva@gmail.com>
2022-11-15 11:56:31 +0530

    Changed from "userid or username" to "restaurant_id and username" login system. Restaurant_id being stored in localStorage for subsequent access

--- src/lib/login.svelte
+++ src/lib/login.svelte
@@ -1,15 +1,47 @@
 <script>
     const url = import.meta.env.VITE_URL;
     // Svelte Store Import
-    import {auth } from "$lib/stores";
+    import { auth } from "$lib/stores";
     import { goto } from "$app/navigation";
 
-    import { postFetch } from "$lib/fetch";
+    import { postFetch, getFetch } from "$lib/fetch";
+    import { onMount } from "svelte";
+
+    let isRestaurantIDSet = false;
+
+    let restaurantName = null;
+
+    let restaurantLocation = null;
+
+    let restaurantID = null;
+
+    onMount(async () => {
+        let restaurant_id = localStorage.getItem("restaurant_id");
+
+        if (restaurant_id != null) {
+            isRestaurantIDSet = true;
+
+            restaurantID = restaurant_id;
+
+            const data = await getFetch(
+                `${url}/api/restaurant/${restaurant_id}`
+            );
+
+            if (data.message == "READ_RESTAURANT_SUCCEEDED") {
+                restaurantName = data.data["restaurant"]["restaurant_name"];
+                restaurantLocation = data.data["restaurant"]["location"];
+            }
+        }
+    });
 
     async function login(e) {
+        if (!isRestaurantIDSet) {
+            restaurantID = document.getElementById("restaurant_id").value;
+        }
+
         const credentials = {
-            userid: document.getElementById("userid").value,
-            username: document.getElementById("username").value,
+            restaurant_id: restaurantID,
+            username: document.getElementById("username").value.toLowerCase(),
             password: document.getElementById("password").value,
         };
 
@@ -21,11 +53,23 @@
 
         const data = await postFetch(`${url}/api/login`, formData);
 
-        if (data) {
+        if (data.message === "LOGGED_IN") {
+            if (!isRestaurantIDSet) {
+                localStorage.setItem(
+                    "restaurant_id",
+                    document.getElementById("restaurant_id").value
+                );
+            }
+
             auth.set(data.message);
-            goto('/');
+            goto("/");
             return;
         }
+    }
+
+    function notYours() {
+        localStorage.removeItem("restaurant_id");
+        isRestaurantIDSet = false;
     }
 </script>
 
@@ -34,23 +78,33 @@
     method="POST"
     class="container flex h-full flex-col items-center gap-7 py-8"
 >
-
-    <!-- User ID -->
-    <div class="flex w-3/4 flex-col justify-between">
-        <label for="userid" class="text-xl font-semibold">User ID</label>
-        <input
-            type="text"
-            name="userid"
-            id="userid"
-            class="h-8 outline outline-2 outline-black md:h-10 lg:h-12"
-        />
-    </div>
-
-    <div>
-        <span class="font-mono tracking-wide">
-            OR
-        </span>
-    </div>
+    {#if isRestaurantIDSet}
+        <div class="flex flex-row gap-3">
+            <span class="text-md font-semibold">{restaurantName}</span>
+            <span class="text-sm font-normal">{restaurantLocation}</span>
+            <hr />
+            <button
+                class="outline outline-2 outline-offset-2"
+                on:click={notYours}
+            >
+                Not yours?
+            </button>
+        </div>
+    {:else}
+        <!-- Restaurant ID -->
+        <div class="flex w-3/4 flex-col justify-between">
+            <label for="restaurant_id" class="text-xl font-semibold"
+                >Restaurant ID</label
+            >
+            <input
+                type="text"
+                name="restaurant_id"
+                id="restaurant_id"
+                class="h-8 outline outline-2 outline-black md:h-10 lg:h-12"
+                placeholder="Restaurant ID"
+            />
+        </div>
+    {/if}
 
     <!-- Username -->
     <div class="flex w-3/4 flex-col justify-between">
@@ -60,6 +114,8 @@
             name="username"
             id="username"
             class="h-8 outline outline-2 outline-black md:h-10 lg:h-12"
+            placeholder="Username"
+            required
         />
     </div>
 
@@ -71,6 +127,7 @@
             name="password"
             id="password"
             class="h-8 outline outline-2 outline-black md:h-10 lg:h-12"
+            placeholder="Password"
         />
     </div>
 
