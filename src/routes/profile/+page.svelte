9546080fabb3b5ee9146a21f0848501761a80e6f
hanani8 <hananidasilva@gmail.com>
2022-12-20 11:44:13 +0530

    Removed "add_edit_reservation" and "login" components. Made (admin), (user), (superadmin) route-groups.

--- src/routes/profile/+page.svelte
+++
@@ -1,5 +0,0 @@
-<div class="container">
-    <p class="text-semibold text-7xl">Ajay Mallareddy</p>
-    <p class="text-bold text-3xl">Flying Spaghetii Monster</p>
-    <p class="text-medium text-xl">Jubilee Hills</p>
-</div>
